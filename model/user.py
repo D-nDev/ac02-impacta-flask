0013ba51245f23805069b4e6272b6c4f04fee3a1
D-nDev <diego-s.novaes@hotmail.com>
2022-05-05 02:27:46 -0300

    feat(release): release v1

---
+++ model/user.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(100), nullable=False)
+    user_email = db.Column(db.String(100), nullable=False)
+    user_address = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.user_name
